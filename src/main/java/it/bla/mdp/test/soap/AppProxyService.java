f2ca2498ee476a47d437bc531ba42475c25a5eec
simone.celia <simone.celia@eng.it>
2023-11-28 14:37:10 +0100

    init

--- src/main/java/it/bla/mdp/test/soap/AppProxyService.java
+++ src/main/java/it/bla/mdp/test/soap/AppProxyService.java
@@ -4,7 +4,7 @@ import javax.jws.WebMethod;
 import javax.jws.WebService;
 
 
-//@WebService ( name = "AppProxyService", serviceName = "AppProxyService" ) // if comment this one it works (HealthCheckTest.http as test)
+@WebService ( name = "AppProxyService", serviceName = "AppProxyService" ) // if comment this one it works (HealthCheckTest.http as test)
 public interface AppProxyService {
 
 	@WebMethod
